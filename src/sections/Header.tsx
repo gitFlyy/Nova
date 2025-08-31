1461775e67e3896b2dc810557b4a1192fbf170fb
gitFlyy <your-email@example.com>
2025-08-31 10:19:00 +0500

    header complete

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,9 +4,11 @@ import Button from "@/components/button";
 
 export const Header = () => {
   return (
-    <header className="py-4 border-b border-white/10 md:border-none">
+    <header className="py-4 border-b border-white/10 md:border-none sticky top-0 z-10">
+      <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div>
       <div className="px-7 md:container">
-        <div className="flex justify-between items-center md:border border-white/5 md:p-2.5 rounded-xl max-w-2xl mx-auto">
+        <div className="flex justify-between items-center md:border border-white/5 md:p-2.5 rounded-xl max-w-2xl mx-auto relative">
+        <div className="absolute inset-0 backdrop-blur -z-10 hidden md:block"></div>
           <div>
             <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/10">
               <LogoIcon className="h-8 w-8" />
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,27 @@
+import acmeLogo from "@/assets/logo-acme.png"
+import apexLogo from "@/assets/logo-apex.png"
+import celestialLogo from "@/assets/logo-celestial.png"
+import quantumLogo from "@/assets/logo-quantum.png"
+import echoLogo from "@/assets/logo-echo.png"
+import pulseLogo from "@/assets/logo-pulse.png"
+
 export const LogoTicker = () => {
-  return <section>LogoTicker</section>;
+  return (
+    <section className="py-20">
+      <div className="container px-0 md:px-4">
+        <div className="flex items-center gap-5">
+          <div className="flex-1 md:flex-none">
+            <h2>Trusted by top innovative firms</h2>
+          </div>
+          <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+            <div className="flex flex-none gap-14 ">
+              {[acmeLogo, pulseLogo, echoLogo, celestialLogo, apexLogo, quantumLogo].map((logo) => (
+                <img src={logo.src} key={logo.src} className="h-6 w-auto" />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
